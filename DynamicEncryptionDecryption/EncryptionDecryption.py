303fe77d94d73f059703a87adcc5d2adbdbdc190
ven0my <vaibhavm391@gmail.com>
2021-01-26 14:40:46 +0530

    scripts updated

---
+++ DynamicEncryptionDecryption/EncryptionDecryption.py
@@ -0,0 +1,138 @@
+#!/usr/bin/python3
+
+"""
+@author: Vaibhav Mishra
+"""
+from Crypto.Hash import MD5
+from Crypto.Cipher import AES
+from Crypto import Random
+import os
+import sys
+
+
+class DynamicEncryptionAndDecryption(object):
+    def __init__(self, filename=None):
+        self.filename = filename
+
+    def encrypt(self, key, filename):
+        chunksize = 128 * 1024
+        outFile = os.path.join(os.path.dirname(filename), "(Secured)" + os.path.basename(filename))
+        filesize = str(os.path.getsize(filename)).zfill(16)
+        IV = Random.new().read(AES.block_size)
+        print(IV, len(IV))
+        encryptor = AES.new(key, AES.MODE_CBC, IV)
+
+        with open(filename, "rb") as infile:
+            with open(outFile, "wb") as outfile:
+                outfile.write(filesize.encode('utf-8'))
+                outfile.write(IV)
+                while True:
+                    chunk = infile.read(chunksize)
+                    if len(chunk) == 0:
+                        break
+                    elif len(chunk) % 16 != 0:
+                        chunk += b' ' * (16 - (len(chunk) % 16))
+                    outfile.write(encryptor.encrypt(chunk))
+        return outFile
+
+    def decrypt(self, key, filename):
+        outFile = os.path.join(os.path.dirname(filename),
+                               os.path.basename(filename).replace("(Secured)", ""))
+        print(outFile)
+        chunksize = 128 * 1024
+        with open(filename, "rb") as infile:
+            filesize = infile.read(16)
+            IV = infile.read(16)
+            decryptor = AES.new(key, AES.MODE_CBC, IV)
+
+            with open(outFile, "wb") as outfile:
+                while True:
+                    chunk = infile.read(chunksize)
+                    if len(chunk) == 0:
+                        break
+                    outfile.write(decryptor.decrypt(chunk))
+                outfile.truncate(int(filesize))
+        return outFile
+
+    @staticmethod
+    def allfiles(path=os.getcwd()):
+        allFiles = []
+        for root, subfiles, files in os.walk(path):
+            for dir_name in subfiles:
+                allFiles.append(os.path.join(root, dir_name))
+            for file_name in files:
+                allFiles.append(os.path.join(root, file_name))
+        return allFiles
+
+
+def choices():
+    ed_object = DynamicEncryptionAndDecryption()
+    choice = input("Do you want to L - List the Files, E - Encrypt or D - Decrypt? ==")
+    print("\n")
+    perform_multiple_encryption = input(f"Do you want to perform multi-layered encryption? Y-Yes or N-No: ")
+    if perform_multiple_encryption.lower() not in ("yes", "y"):
+        perform_multiple_encryption = False
+        password = input("Please enter the `Password/Key` to be used: ")
+    else:
+        encFiles = ed_object.allfiles()
+
+    if choice == "E":
+        print("")
+        subchoice = input("Want to encrypt all the Files ? Y- Yes or N - No ? :")
+        if subchoice == "Y":
+            for Tfiles in encFiles:
+                if os.path.basename(Tfiles).startswith("(Secured)"):
+                    print(f"{Tfiles} is already encrypted")
+                    pass
+                elif Tfiles == os.path.join(os.getcwd(), sys.argv[0]):
+                    pass
+                else:
+                    ed_object.encrypt(MD5.new(password).digest(), str(Tfiles))
+                    print(f"Done Encryption for {Tfiles}")
+                    os.remove(Tfiles)
+        elif subchoice == "N":
+            print("")
+            filename = input("Enter the Filename to Encrypt: ")
+            if not os.path.exists(filename):
+                print(f"Given file {filename} does not exist")
+                sys.exit(0)
+            elif filename.startswith("(Secured)"):
+                print(f"File {filename} was already encrypted")
+                sys.exit()
+            else:
+                ed_object.encrypt(MD5.new(password).digest(), filename)
+                print(f"Done Encryption of {filename}")
+                os.remove(filename)
+        else:
+            print("\n Enter either Y or N")
+
+    elif choice == "D":
+        print("")
+        filename = input("Enter the filename to decrypt: ")
+        if not os.path.exists(filename):
+            print(f"Given file {filename} does not exist")
+            sys.exit(0)
+        elif not filename.startswith("(Secured)"):
+            print("{filename} file was never encrypted")
+            sys.exit()
+        else:
+            ed_object.decrypt(MD5.new(password).digest(), filename)
+            print(f"Done Decryption for {filename}")
+            os.remove(filename)
+
+    elif choice == "L":
+        print(" \n The files present in the current directory are: ")
+        file_list = []
+        for content in os.listdir(".."):
+            file_list.append(content)
+        print(file_list)
+
+    else:
+        print("\n Please choose a valid command. Either E or D")
+        sys.exit()
+
+
+if __name__ == "__main__":
+    # choices()
+    pass
+
---
+++ app.py
@@ -0,0 +1,131 @@
+#!/usr/bin/python3
+
+import os
+from flask import Flask, request, render_template, session, redirect, url_for, flash
+from DynamicEncryptionDecryption import EncryptionDecryption
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(32)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def home_page():
+    if request.method == 'POST':
+        if all([key in request.form for key in ('fname', 'pwd', 'options')]):
+            file_name = "?".join([str(ord(char)) for char in request.form['fname']])
+            global password
+            password = request.form['pwd']
+            print(file_name, password)
+            option = request.form['options']
+            if file_name and password and option:
+                if option.lower() == "encrypt":
+                    return redirect(url_for('encryption', f_name=file_name))
+                elif option.lower() == "decrypt":
+                    return redirect(url_for('decryption', f_name=file_name))
+    return render_template('home.html')
+
+
+@app.route("/encrypt/<string:f_name>", methods=['GET', 'POST'])
+def encryption(f_name=None):
+    file_name = "".join(list(map(chr, list(map(int, f_name.split("?"))))))
+    print(file_name)
+    del_option = None
+    if request.method == "POST":
+        del_option, session['encryption_file_option'] = [request.form['deloptions']]*2
+        ed_object = EncryptionDecryption.DynamicEncryptionAndDecryption()
+        if not os.path.exists(file_name):
+            flash(f"Given file {file_name} does not exist", "#800080")
+        elif file_name.startswith("(Secured)"):
+            flash(f"File {file_name} was already encrypted", "#800080")
+        else:
+
+            def file_encryption(file_name):
+                output_file = ed_object.encrypt("{: <32}".format(password).encode(), file_name)
+                flash(f"Done Encryption of {file_name}", "#00008B")
+                flash(f"The Encrypted file name is {output_file}", "#006400")
+                if del_option == "True":
+                    os.remove(file_name)
+                    flash(f"The file - {file_name} has been removed", "#2F4F4F")
+                return
+            if os.path.isfile(file_name):
+                file_encryption(file_name)
+            elif os.path.isdir(file_name):
+                folder_files = ed_object.allfiles(file_name)
+                print(folder_files)
+                for Tfile in folder_files:
+                    if os.path.isfile(Tfile):
+                        file_encryption(Tfile)
+                for Tfile in folder_files:
+                    if os.path.isdir(Tfile):
+                        os.rename(Tfile, Tfile + "(Secured)")
+                os.rename(file_name, file_name + "(Secured)")
+            else:
+                pass
+
+    return render_template('enc_result.html', del_option=del_option)
+
+
+@app.route("/decrypt/<string:f_name>", methods=["POST", "GET"])
+def decryption(f_name=None):
+    file_name = "".join(list(map(chr, list(map(int, f_name.split("?"))))))
+    print(file_name)
+    del_option = None
+    if request.method == "POST":
+        del_option, session['encryption_file_option'] = [request.form['deloptions']] * 2
+        ed_object = EncryptionDecryption.DynamicEncryptionAndDecryption()
+        if not os.path.exists(file_name):
+            flash(f"Given file {file_name} does not exist", "#800080")
+        elif os.path.isfile(file_name) and not file_name.split("/")[-1].startswith("(Secured)"):
+            flash(f"File {file_name} was NOT encrypted", "#800080")
+        elif os.path.isdir(file_name) and not file_name.split("/")[-1].endswith("(Secured)"):
+            flash(f"File {file_name} was NOT encrypted", "#800080")
+        else:
+
+            def file_decryption(file_name):
+                output_file = ed_object.decrypt("{: <32}".format(password).encode(), file_name)
+                flash(f"Done Deryption of {file_name}", "#00008B")
+                flash(f"The Decrypted file name is {output_file}", "#006400")
+                if del_option == "True":
+                    os.remove(file_name)
+                    flash(f"The file - {file_name} has been removed", "#2F4F4F")
+                return
+            if os.path.isfile(file_name):
+                file_decryption(file_name)
+            elif os.path.isdir(file_name):
+                folder_files = ed_object.allfiles(file_name)
+                print(folder_files)
+                for Tfile in folder_files:
+                    if os.path.isfile(Tfile):
+                        file_decryption(Tfile)
+                for Tfile in folder_files:
+                    if os.path.isdir(Tfile):
+                        parent_path = os.path.dirname(Tfile)
+                        base_path = os.path.basename(Tfile).replace("(Secured)", '')
+                        os.rename(Tfile, os.path.join(parent_path, base_path))
+                os.rename(file_name, file_name.replace("(Secured)", ''))
+            else:
+                pass
+    return render_template('dec_result.html', del_option=del_option)
+
+
+@app.route("/listfiles", methods=['GET', 'POST'])
+def list_files():
+    ed_object = EncryptionDecryption.DynamicEncryptionAndDecryption()
+    if request.method == "GET":
+        available_files = ed_object.allfiles()
+        default_folder = "Default Folder"
+    elif request.method == "POST":
+        path_name = request.form['foldername']
+        available_files = ed_object.allfiles(path=path_name)
+        default_folder = path_name
+    else:
+        return "Invalid HTTP Request"
+    available_files = sorted(available_files)
+    return render_template('list_files.html',
+                           all_avail_files=available_files,
+                           default_folder=default_folder)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port="5000", debug=True)
